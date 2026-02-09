6dabbcd7eaef37bc9fadbc8fd41cb2ebe5f33576
Sahana <sahananarayana21@gmail.com>
2026-02-09 11:17:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 let alarms = []; // { level: number, sound: string }
 let alertedLevels = new Set();
+
+const statusText = document.getElementById("battery-status");
+const message = document.getElementById("message");
